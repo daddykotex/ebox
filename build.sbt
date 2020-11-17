b60badfcf18007ea58bcee5e1a645edf9c66f6b9
David Francoeur <dfrancoeur04@gmail.com>
2020-11-16 21:37:21 -0500

    feat: dummy get-usage command using decline

--- build.sbt
+++ build.sbt
@@ -1,18 +1,21 @@
-lazy val root = (project in file(".")).
-  settings(
-    inThisBuild(List(
+lazy val root = (project in file(".")).settings(
+  inThisBuild(
+    List(
       organization := "com.daddykotex",
       scalaVersion := "2.13.3"
-    )),
-    name := "ebox"
-  )
-
-
-lazy val eboxCli = (project in file("ebox-cli")).
-  settings(
-    name := "ebox-cli",
-    libraryDependencies ++= Seq(
-      "org.typelevel" %% "cats-core" % "2.2.0",
-      "org.http4s" %% "http4s-blaze-client" % "0.21.8"
     )
+  ),
+  name := "ebox"
+)
+
+lazy val declineVersion = "1.3.0"
+
+lazy val eboxCli = (project in file("ebox-cli")).settings(
+  name := "ebox-cli",
+  libraryDependencies ++= Seq(
+    "org.typelevel" %% "cats-core" % "2.2.0",
+    "org.http4s" %% "http4s-blaze-client" % "0.21.8",
+    "com.monovore" %% "decline" % declineVersion,
+    "com.monovore" %% "decline-effect" % declineVersion
   )
+)
--- ebox-cli/src/main/scala/main.scala
+++ ebox-cli/src/main/scala/main.scala
@@ -1,3 +1,34 @@
-object Main extends App {
-  println("Hey")
+import cats.effect._
+import cats.implicits._
+
+import com.monovore.decline._
+import com.monovore.decline.effect._
+
+//Opts
+final case class Credentials(accountNumber: String, password: String)
+
+// Commands
+final case class GetUsage(credentials: Credentials)
+
+object Main
+    extends CommandIOApp(
+      name = "ebox-cli",
+      header = "Simple CLI tool get info on your ebox account",
+      version = "0.0.x"
+    ) {
+
+  val accountNameOpts: Opts[String] = Opts.argument[String](metavar = "account-number")
+  val passwordOpts: Opts[String] = Opts.argument[String](metavar = "password")
+  val credentialsOpts: Opts[Credentials] = (accountNameOpts, passwordOpts).mapN(Credentials)
+
+  // Commands
+  val getUsageCmd: Opts[GetUsage] =
+    Opts.subcommand("get-usage", "Returns a percentage of your bandwidth usage.") {
+      credentialsOpts.map(GetUsage)
+    }
+
+  override def main: Opts[IO[ExitCode]] =
+    getUsageCmd.map { case GetUsage(all) =>
+      IO.delay(println("50")).as(ExitCode.Success)
+    }
 }
